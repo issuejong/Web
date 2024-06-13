74bb5cafd692ea8c9d26c9cac8df5c6108db9017
이수종 <137382468+issuejong@users.noreply.github.com>
2024-06-13 23:56:46 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function atag(){
   ab.setColor('red');
 }
+
+function before(name) {
+  if(name === 'lilly')
+    document.getElementById("beforeid").innerHTML = "호주 빅토리아주 메리즈빌(Marysville)에서 태어나고 자랐다.";
+  else if(name === 'haewon')    
+    document.getElementById("beforeid").innerHTML = "2017년 열린 'JYP 걸그룹 공개 오디션'에 합격해 입사했다. # 개최 장소와 기간이 변경될 정도로 지원자가 폭주했던 오디션으로, 약 9천여 명이 지원했다고 알려져 있다.# 오디션 곡은 비욘세의 Halo였는데, 오디션장에 들어가 해보라는 신호가 나오자마자 'Everywhere I'm lookin' now~'부터 이어지는 하이라이트를 불렀다고 한다.[9]";
+}
