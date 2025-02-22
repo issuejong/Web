4743406f1a737eb6ba4ee296444c3e14430fda33
이수종 <137382468+issuejong@users.noreply.github.com>
2025-02-22 20:05:46 +0900

    Update todo_script.js

--- ToDoList/todo_script.js
+++ ToDoList/todo_script.js
@@ -3,10 +3,13 @@ $(document).ready(function(){
    //생성
   $("#add").click(function(){
     input = $("input").val();
-    var trClone = $("#default_plan").clone();
-    trClone.css("display", "");
-    $(trClone).find(".plan").text(input);
-    $("#plan_list").append(trClone);
+    if(input !== "") 
+    {
+      var trClone = $("#default_plan").clone();
+      trClone.css("display", "");
+      $(trClone).find(".plan").text(input);
+      $("#plan_list").append(trClone);
+    }
   });
   
    //on으로 동적 이벤트 생성하여 삭제하기
